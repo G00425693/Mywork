990840becc2e98c4d34fd597dc055194296dd947
G00425693 <G00425693@ATU.IE>
2023-02-18 15:11:13 +0000

    mywork

---
+++ week2/bank.py
@@ -0,0 +1,13 @@
+# bank question
+# Author Audrey Fitzgerald
+# use integer rather than float
+
+print ("enter your amount")
+x = int(input())
+a = x/100
+print ("enter your amount")
+y = int(input())
+b = y/100
+ans = a + b
+print("€", ans)
+
